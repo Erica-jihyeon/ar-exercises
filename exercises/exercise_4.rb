e6b6f8a6bc2f4c14e7ebfa591090942b347c46cc
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-31 22:05:01 +0000

    add new stores and filter men's apparel stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,29 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Stores.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+store = Stores.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+store = Stores.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Stores.where(mens_apparel: true)
+
+@mens_stores.each do |s|
+  puts "store name: #{s.name} | annual revenue: #{s.annual_revenue}"
+end
