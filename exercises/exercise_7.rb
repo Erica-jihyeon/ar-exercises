e4fc81bce38a91a2e01c267472efc33b04a25cd9
Erica-jihyeon <jihyeon20@gmail.com>
2022-02-01 04:01:21 +0000

    get input from the user and show the validation error for creating stores

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,17 +9,39 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "What is your store name?"
+input_store_name = $stdin.gets.chomp
 
-@store1.employees.create(first_name: "hello", last_name: "Virani", hourly_rate: 10)
+store = Store.create(
+  name: "Gassss",
+  mens_apparel: false,
+  womens_apparel: false
+)
+
+if store.errors.any? # If there are errors, do something
+  store.errors.each do |attribute, message|
+    p "#{attribute} #{message}"
+  end
+end
+
+# same as Store.create
+# store = Store.new
+# store.name = input_store_name
+# store.mens_apparel = false
+# store.womens_apparel = false
+# store.save
+
+
+# validation test code
+# @store1.employees.create(first_name: "hello", last_name: "Virani", hourly_rate: 10)
 
 # employee = Employee.create(
 #   first_name: "hello", last_name: "Virani", hourly_rate: 50
 # )
 
-store = Store.create(
-  name: "Gassss",
-  annual_revenue: 20,
-  mens_apparel: false,
-  womens_apparel: false
-)
+# store = Store.create(
+#   name: "Gassss",
+#   annual_revenue: 20,
+#   mens_apparel: false,
+#   womens_apparel: false
+# )
